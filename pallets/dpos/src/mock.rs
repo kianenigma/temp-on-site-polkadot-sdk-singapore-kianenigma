ffaadefffda1a6dcc01419896721b2afafb7e3b8
Shawn Tabrizi <shawntabrizi@gmail.com>
2024-06-10 16:53:02 +0800

    cargo fmt

--- pallets/dpos/src/mock.rs
+++ pallets/dpos/src/mock.rs
@@ -109,17 +109,17 @@ pub fn new_test_ext() -> sp_io::TestExternalities {
 }
 
 pub fn next_block() {
-    System::set_block_number(System::block_number() + 1);
-    System::on_initialize(System::block_number());
-    Dpos::on_initialize(System::block_number());
+	System::set_block_number(System::block_number() + 1);
+	System::on_initialize(System::block_number());
+	Dpos::on_initialize(System::block_number());
 }
 
 pub fn run_to_block(n: BlockNumberFor<Test>) {
-    while System::block_number() < n {
-        if System::block_number() > 1 {
-            Dpos::on_finalize(System::block_number());
-            System::on_finalize(System::block_number());
-        }
-        next_block();
-    }
+	while System::block_number() < n {
+		if System::block_number() > 1 {
+			Dpos::on_finalize(System::block_number());
+			System::on_finalize(System::block_number());
+		}
+		next_block();
+	}
 }
